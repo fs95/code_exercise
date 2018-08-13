152ca753fd23052f0e97617b55a188a555e42c57
fengshun <fengshun95@gmail.com>
2018-08-13 22:09:00 +0800

    add counter.sh

--- subsection01/counter.sh
+++ subsection01/counter.sh
@@ -1,4 +1,36 @@
 #!/bin/bash
 
-# wc 统计文件字节数量
+# wc file | awk '{print $3}' # 输出第三列的字节数
 
+# file_count=0;
+function traverse {
+
+    size=0 # 目录里的总大小
+    if [ $# -eq 1 ]; then
+
+        dirctory=$1
+        for file in $dirctory/*; do
+
+            if [ -d "$file" ]; then # 目录，递归
+                size=$[$size + $(traverse $file)]
+
+            elif [ -f "$file" ]; then # 文件，记录大小
+                file_size=$(wc -c $file | awk '{print $1}') # wc -c输出总字节数（总大小）
+                size=$[$size + $file_size]
+                # file_count=$[$file_count + 1]
+                # echo "file_counter: $file_count $file: FILE"
+            fi
+        done
+    fi
+    echo $size
+}
+
+if [ $# -ne 1 ] || [ ! -d $1 ]; then # 参数数量！=1，或者第一个参数不是目录或不存在
+    echo "counter: Need parameter"
+    echo "usage: counter DIR"
+    exit 1
+else
+    file=$1
+    size=$(traverse $file)
+    echo "The total size of the $1 directory: $size[byte]"
+fi
--- subsection01/fibonacci.sh
+++ subsection01/fibonacci.sh
@@ -3,7 +3,7 @@
 # 表达式：
 # var1=$[$var1+1]
 
-if [ $# -ne 0 ] ; then # 有参数
+if [ $# -ne 0 ]; then # 有参数
 
     if [ $1 == "--clear" ]; then
         rm 00[0-4][0-9].bin 2> /dev/null
@@ -24,8 +24,6 @@ cat /dev/urandom | head -c 1 > $file_name_2
 for (( i = 3; i <= 45; i++)); do
 
     file_name=00$[$i/10]$[$i%10].bin # 生成对应文件名
-    touch $file_name # 创建文件
-
     cat $file_name_1 > $file_name # 复制
     cat $file_name_2 >> $file_name # 追加
 
