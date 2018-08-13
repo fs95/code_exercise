60f0ac126c148e25047cab67a6d44c837eca2c68
fengshun <fengshun95@gmail.com>
2018-08-13 16:40:23 +0800

    Add '--clear'

--- subsection01/fibonacci.sh
+++ subsection01/fibonacci.sh
@@ -3,6 +3,18 @@
 # 表达式：
 # var1=$[$var1+1]
 
+if [ $# -ne 0 ] ; then # 有参数
+
+    if [ $1 == "--clear" ]; then
+        rm 00[0-4][0-9].bin 2> /dev/null
+        exit 0
+    else
+        echo "invalid option"
+        echo "usage: fibonacci [--clear]"
+        exit 1
+    fi
+fi
+
 # 初始化
 file_name_1='0001.bin'
 file_name_2='0002.bin'
@@ -14,7 +26,7 @@ for (( i = 3; i <= 45; i++)); do
     file_name=00$[$i/10]$[$i%10].bin # 生成对应文件名
     touch $file_name # 创建文件
 
-    cp $file_name_1 $file_name # 复制
+    cat $file_name_1 > $file_name # 复制
     cat $file_name_2 >> $file_name # 追加
 
     # 循环赋值
