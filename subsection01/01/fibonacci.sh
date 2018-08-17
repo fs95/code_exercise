6783890e5ff7df4eb5fbc681f808343f1618f2ee
fs95 <fengshun95@gmail.com>
2018-08-17 11:04:42 +0800

    Replace the comment in subsection01 with English

--- subsection01/01/fibonacci.sh
+++ subsection01/01/fibonacci.sh
@@ -1,33 +1,31 @@
 #!/bin/bash
 
-# 表达式：
-# var1=$[$var1+1]
+# If there are parameters
+if [ $# -ne 0 ]; then
 
-if [ $# -ne 0 ]; then # 有参数
-
-    if [ $1 == "--clear" ]; then
+    if [ $1 == "--clean" ]; then
         rm 00[0-4][0-9].bin 2> /dev/null
         exit 0
     else
         echo "invalid option"
-        echo "usage: fibonacci [--clear]"
+        echo "usage: fibonacci [--clean]"
         exit 1
     fi
 fi
 
-# 初始化
+# Initalize
 file_name_1='0001.bin'
 file_name_2='0002.bin'
-cat /dev/urandom | head -c 1 > $file_name_1 # 写入固定数量的随机字符
+cat /dev/urandom | head -c 1 > $file_name_1 # Write a random character
 cat /dev/urandom | head -c 1 > $file_name_2
 
 for (( i = 3; i <= 45; i++)); do
 
-    file_name=00$[$i/10]$[$i%10].bin # 生成对应文件名
-    cat $file_name_1 > $file_name # 复制
-    cat $file_name_2 >> $file_name # 追加
+    file_name=00$[$i/10]$[$i%10].bin # Generate matching files
+    cat $file_name_1 > $file_name # Copy
+    cat $file_name_2 >> $file_name # Append
 
-    # 循环赋值
+    # Cyclic assignment
     file_name_2=$file_name_1
     file_name_1=$file_name
 
--- subsection01/02/counter.sh
+++ subsection01/02/counter.sh
@@ -1,42 +1,44 @@
 #!/bin/bash
-# 功能：遍历一个目录得到总字节大小和其中大于1MB字节大小的文件数量
-# wc file | awk '{print $3}' # 输出第三列的字节数
+# Function: Traverse a directory to get the total byte size
+# and the number of files large than 1MB in size
+# Skill: wc file | awk '{print $3}' # Output the number of bytes in the third column
 
-# 遍历一个目录 usage: temp=($(traverse $file))
+# Usage: temp=($(traverse $DIR))
 function traverse {
 
-    size=0 # 目录里的总大小
-    large_then1M=0 # 超过1M的文件数量
+    size=0 # Total size of the directory
+    large_then1M=0 # Number of files over 1MB
     if [ $# -eq 1 ]; then
 
         directory=$1
         for file in $directory/*; do
 
-            if [ -d "$file" ]; then # 目录，递归
+            if [ -d "$file" ]; then # Is a directory, recursive
                 temp=($(traverse $file))
                 size=$[$size + ${temp[0]}]
                 large_then1M=$[$large_then1M + ${temp[1]}]
 
-            elif [ -f "$file" ]; then # 文件，记录大小
-                file_size=$(wc -c $file | awk '{print $1}') # wc -c输出总字节数（总大小）
+            elif [ -f "$file" ]; then # Is a file, the record size
+                file_size=$(wc -c $file | awk '{print $1}') # wc -c,output total bytes
                 size=$[$size + $file_size]
-                if [ $file_size -gt $[1024 * 1024] ]; then # 大小超过1MB
+                if [ $file_size -gt $[1024 * 1024] ]; then # More than 1MB in size
                     large_then1M=$[$large_then1M + 1]
                 fi
             fi
         done
     fi
-    # temp=($size $large_then1M); echo ${temp[*]} # 返回数组的方式
     echo $size $large_then1M
+    # temp=($size $large_then1M); echo ${temp[*]} # Return using array mode
 }
 
-if [ $# -ne 1 ] || [ ! -d $1 ]; then # 参数数量！=1，或者第一个参数不是目录或不存在
+# Number of parameters != 1, or the first parameter is not a directory or does not exist
+if [ $# -ne 1 ] || [ ! -d $1 ]; then 
     echo "Error: Need parameter"
     echo "Usage: counter DIR"
     exit 1
 else
     file=$1
-    temp=($(traverse $file)) # 得到两个数组变量
+    temp=($(traverse $file)) # Get two array variables
     size=${temp[0]}
     large_then1M=${temp[1]}
     echo "The total size of the $1 directory: $size[byte]"
