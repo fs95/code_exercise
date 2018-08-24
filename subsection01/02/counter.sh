5bb4454b20d949ef9cd2104da5703251085a026b
feng shun <fengshun95@gmail.com>
2018-08-24 12:02:36 +0800

    Update method

--- subsection01/02/counter.sh
+++ subsection01/02/counter.sh
@@ -8,7 +8,8 @@ else
 fi
 file=$1
 limitFileSize=$[1000 * 1000]
-size=$(du -bs $1 | awk '{print $1}')
-large_then1M=$(ls -Ral $path | grep "^-" | gawk '$5>=('"$limitFileSize"')' | wc -l)
+# size=$(du -bs $1 | awk '{print $1}')
+size=$(ls -Ral $1 | gawk '$1 ~ /^-/ {sum+=$5};END{print sum;}')
+large_then1M=$(ls -Ral $path | gawk '$1 ~ /^-/ $5>=('"$limitFileSize"')' | wc -l)
 echo "The total size of the [$path] directory: $size[byte]"
 echo "The number of files larger than $limitFileSize[byte]: $large_then1M"
